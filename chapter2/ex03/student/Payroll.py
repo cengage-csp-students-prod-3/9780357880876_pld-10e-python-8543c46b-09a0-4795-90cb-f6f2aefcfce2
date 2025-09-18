dd9e370bcc57c95e14804cb59c6084cf0eef24f0
jaredFbaby <69921626+jaredFbaby@users.noreply.github.com>
2025-09-18 00:16:45 +0000

    Testing Thu Sep 18 2025 00:16:45 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/Payroll.py
+++ chapter2/ex03/student/Payroll.py
@@ -3,7 +3,7 @@ salary = 1250.00
 numDependents = 2
 stateTax = .065 * salary
 federalTax = .28 * salary
-dependentDeduction = .025 * salary *numDependents
+dependentDeduction = .025 * salary *numDependent
 totalWithholding = stateTax + federalTax
 takeHomePay = salary - totalWithholding + dependentDeduction
 
