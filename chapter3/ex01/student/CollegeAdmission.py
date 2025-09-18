8f1015118faf5a50349fbb7458e6b04d158eb365
jaredFbaby <69921626+jaredFbaby@users.noreply.github.com>
2025-09-18 01:16:15 +0000

    Testing Thu Sep 18 2025 01:16:15 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex01/student/CollegeAdmission.py
+++ chapter3/ex01/student/CollegeAdmission.py
@@ -7,7 +7,7 @@
 # Get input and convert to correct data type
 testScoreString = input ("Enter student's test score: ")
 test_score = int (testScoreString)
-classRankString = input ("enter student's class Rank:")
+classRankString = input ("enter student's class Rank:"
 class_rank = int (classRankString)
 # Test using admission requirements and print Accept or Reject
 
