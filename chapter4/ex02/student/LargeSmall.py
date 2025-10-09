60fac58ec2d20483df2c964405369244e345ac95
jaredFbaby <69921626+jaredFbaby@users.noreply.github.com>
2025-10-09 00:53:45 +0000

    Testing Thu Oct 09 2025 00:53:45 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex02/student/LargeSmall.py
+++ chapter4/ex02/student/LargeSmall.py
@@ -4,8 +4,20 @@
 firstNumber = -50
 secondNumber = 53
 thirdNumber = 78
-
 # Write assignment, if, or if else statements here as appropriate.
+if firstNumber > secondNumber and firstNumber > thirdNumber:
+        largest = firstNumber 
+elif secondNumber > firstNumber and secondNumber > thirdNumber:
+     largest = secondNumber
+else :
+    largest = thirdNumber
+
+if firstNumber < secondNumber and firstNumber < thirdNumber:
+    smallest = firstNumber
+elif secondNumber < firstNumber and secondNumber < thirdNumber:
+    smallest = secondNumber
+else :
+    smallest = thirdNumber
 
 # Output largest and smallest number.
 print("The largest value is", largest)
