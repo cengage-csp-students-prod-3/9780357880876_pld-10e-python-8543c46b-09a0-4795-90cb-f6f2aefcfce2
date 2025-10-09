24e4605d2f4c8369ae8eb85226e555ff15178bf4
jaredFbaby <69921626+jaredFbaby@users.noreply.github.com>
2025-10-09 02:34:33 +0000

    Testing Thu Oct 09 2025 02:34:33 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex03/student/EmployeeBonus.py
+++ chapter4/ex03/student/EmployeeBonus.py
@@ -17,13 +17,13 @@ dollar_value = float(dollar_string)
 # Write your code here
 score = (dollar_value / num_transactions) / num_shifts
 if (score  >= 200):
-    bonus = bonus_4
-    if  (score <= 199):
-        bonus = bonus_3
+    bonus = bonus_4;
+    if (score <= 199):
+        bonus = bonus_3;
         if (score <= 69):
-            bonus = bonus_2
-            if (score <=30):
-                bonus= bonus_1
+            bonus = bonus_2;
+            if (score <= 30):
+                bonus = bonus_1;
     
 # Output.
 print("Employee Name:", employee_name)
