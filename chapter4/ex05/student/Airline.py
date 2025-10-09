35fa58741edf02f7cccee2d7203630dffbbe81f9
jaredFbaby <69921626+jaredFbaby@users.noreply.github.com>
2025-10-09 04:11:50 +0000

    Testing Thu Oct 09 2025 04:11:50 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex05/student/Airline.py
+++ chapter4/ex05/student/Airline.py
@@ -1,8 +1,11 @@
-## Airline.py - This program determines if an airline passenger is eligible for a 25% discount.
-
 passengerName = input("Enter passenger's name: ") # Passenger's name.
 ageString = input("Enter passenger's age: ") # string version of passenger's age.
 
 passengerAge = int(ageString) # Passenger's age.
+
+if passengerAge <= 6 or passengerAge >= 65 :   
+    print("Passenger is eligible for a discount.")
+else:
+    print("Passenger is not eligible for a discount.")
 
 # Test to see if this passenger is eligible for a 25% discount.
