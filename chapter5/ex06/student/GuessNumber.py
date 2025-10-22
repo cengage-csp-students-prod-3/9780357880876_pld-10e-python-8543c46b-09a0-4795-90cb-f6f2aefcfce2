a69cf5c1535514e97f65c918e11b6d0dd6284961
jaredFbaby <69921626+jaredFbaby@users.noreply.github.com>
2025-10-22 10:51:34 +0000

    Testing Wed Oct 22 2025 10:51:34 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex06/student/GuessNumber.py
+++ chapter5/ex06/student/GuessNumber.py
@@ -20,7 +20,7 @@ while keepGoing == "Y" :
     userNumber = int(stringNumber)
     
     if userNumber >10 or userNumber < 1:
-        print ("Number must be in the range of 1 to 10: Please try again. ")
+        print ("Number must be in the range of 1 to 10: Please try again: ")
     # Validate input.
 
     # Test to see if the user guessed correctly.
