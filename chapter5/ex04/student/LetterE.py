95e315d74cdb421af603cd893c9e43dba92c9708
jaredFbaby <69921626+jaredFbaby@users.noreply.github.com>
2025-10-22 05:23:02 +0000

    Testing Wed Oct 22 2025 05:23:02 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex04/student/LetterE.py
+++ chapter5/ex04/student/LetterE.py
@@ -4,23 +4,18 @@
 # Output: Prints the letter E.
 
 num_across = 3  # Number of asterisks to print across.
-num_down = 5  # Number of asterisks to print down.
+num_down = 5    # Number of asterisks to print down.
 
 # Write a loop to control the number of rows.
 for row in range(num_down):
-# Write a loop to control the number of columns
+    # Write a loop to control the number of columns.
     for col in range(num_across):
-# Decide when to print an asterisk in every column.
-        if row == 0 or row == 2 or row == 4:
+        # Decide when to print an asterisk or a space based on row and column.
+        if col == 0 or row == 0 or row == num_down // 2 or row == num_down - 1:
             print("*", end="")
-# Decide when to print asterisk in column 1.
-        elif col == 0:
-            print("*", end="")
-
-# Decide when to print a space instead of an asterisk.
         else:
+            # For all other cases, print a space.
             print(" ", end="")
-            print()
-# Figure out where to place this statement that prints a newline.
 
-print()
+    # Place this statement that prints a newline after the inner loop finishes.
+    print()
