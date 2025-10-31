af70f59ba1bff4ae06f79f4e06885307e667ae80
jaredFbaby <69921626+jaredFbaby@users.noreply.github.com>
2025-10-31 15:29:32 +0000

    Testing Fri Oct 31 2025 15:29:32 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex01/student/BattingAverage.py
+++ chapter6/ex01/student/BattingAverage.py
@@ -1,26 +1,17 @@
 # Initialize an integer for list size here.
 
-
+numList = 8
 
 # Initialize list here.
-
-
+average = []
+averageAll=0
 
 # Write a loop to get batting averages from user and add to list.
+for i in range(numList):
+    battingAverageString = input(str("Enter a batting average "))
+    battingAverageFloat = float(battingAverageString)
+    average.append(battingAverageFloat)
 
-
-
-    # String version of batting average input by user.
-
-    averageString = input("Enter a batting average: ")
-
-
-    # Use this variable to store the batting average input by user.
-
-    battingAverage = float(averageString)
-
-
-    # add value to list.
 
 
 
@@ -28,28 +19,35 @@
 
 # Assign the first element in the list to be the minimum and the maximum.
 
-min = averages[0]
+min = average[0]
 
-max = averages[0]
+max = average[0]
 
 
 # Start out your total initialized to 0.
 
-total = 0
-
+total = 0 
 
 # Write a loop here to access list values starting with averages[1]
+for i in average:
+    if i < min:
+        min = i
+    if i > max:
+        max = i
+    total += i
+
 
 
 
     # Within the loop test for minimum and maximum batting averages.
 
 
-
     # Also accumulate a total of all batting averages.
 
 
-
+print("Minimum batting average is " , min)
+print("Maximum batting average is " , max)
+print("Average batting average is ", total)
 
 # Calculate the average of the 8 averages.
 
