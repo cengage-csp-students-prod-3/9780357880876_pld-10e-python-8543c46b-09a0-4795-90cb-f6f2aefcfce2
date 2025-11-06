42b82b1033d3411be37aa13ce983fb110983397e
jaredFbaby <69921626+jaredFbaby@users.noreply.github.com>
2025-11-06 06:20:40 +0000

    Testing Thu Nov 06 2025 06:20:39 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex01/student/ArtShow.py
+++ chapter9/ex01/student/ArtShow.py
@@ -2,13 +2,6 @@
 # for preregistering.
 # Input:  Interactive.
 # Output:  A statement telling the user if they get a discount or no discount.
-def discount():
-	print("You are pre-registered and qualify for a 5% discount.")
-
-# Write noDiscount function here.
-def nodiscount():
-	print("Sorry, you did not pre-register and do not qualify for a 5% discount.")
-
 
 def main():
 
@@ -17,6 +10,15 @@ def main():
 			discount()
 		else:
 			nodiscount()
+
+def discount():
+	print("You are pre-registered and qualify for a 5% discount.")
+
+# Write noDiscount function here.
+def nodiscount():
+	print("Sorry, you did not pre-register and do not qualify for a 5% discount.")
+
+
 		# Test input here. If Y, call discount(), else call noDiscount().
 
 # End of main() function.
